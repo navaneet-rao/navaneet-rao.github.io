ddf37bb1c4abc2c6a8a8cea6ed1056acd7c155fd
Navaneet r rao <76439641+navaneet-rao@users.noreply.github.com>
2023-12-17 19:58:18 +0530

    home page bug fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
+import Work from './components/Work';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Work/>
+      <Contact/>
     </div>
   );
 }
