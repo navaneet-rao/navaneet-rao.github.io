065b7b116e2c7f95110673a249ae670d8c3df9e7
Navaneet r rao <76439641+navaneet-rao@users.noreply.github.com>
2023-12-19 23:12:54 +0530

    fixed contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_tqwe9il",
+        "template_150uil6",
+        form.current,
+        "7NLtyb4zJ35ASR2r4"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div
       name="contact"
@@ -13,9 +34,9 @@ const Contact = () => {
               Contact Me
             </h2>
             <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-              connect with me by filling the below form 
+              Connect with me by filling the below form
             </p>
-            <form action="#" class="space-y-8">
+            <form action="#" class="space-y-8"  ref={form} onSubmit={sendEmail}>
               <div>
                 <label
                   for="email"
@@ -25,9 +46,26 @@ const Contact = () => {
                 </label>
                 <input
                   type="email"
-                  id="email"
+                  id="from_email"
+                  name="from_email"
                   class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                  placeholder="name@flowbite.com"
+                  placeholder="name@gmail.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="subject"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="from_subject"
+                  name="from_name"
+                  class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  placeholder="Full Name"
                   required
                 />
               </div>
@@ -40,9 +78,10 @@ const Contact = () => {
                 </label>
                 <input
                   type="text"
-                  id="subject"
+                  id="from_subject"
+                  name="from_subject"
                   class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                  placeholder="Let us know how we can help you"
+                  placeholder="Subject"
                   required
                 />
               </div>
@@ -56,12 +95,14 @@ const Contact = () => {
                 <textarea
                   id="message"
                   rows="6"
+                  name="message"
                   class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Leave a comment..."
                 ></textarea>
               </div>
               <button
                 type="submit"
+                value="Send"
                 class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg group text-black w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-red-500 to-green-500  cursor-pointer hover:scale-110 duration-500"
               >
                 Send message
