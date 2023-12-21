5bd4d5a67de3d4d0a45bf97b5d9bb2d22990c4d5
Navaneet r rao <76439641+navaneet-rao@users.noreply.github.com>
2023-12-21 23:55:16 +0530

    added svgs

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-
+import ReactLogo from "../assets/icons/react.svg";
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-full bg-[#0a192f] text-gray-200 sm:py-10">
+    <div
+      name="skills"
+      className="w-full h-full bg-[#0a192f] text-gray-200 sm:py-10"
+    >
       {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full  wide:landscape:mt-64">
         <div className=" w-full h-full flex justify-center items-center flex-col mb-7 portrait:mt-40">
@@ -17,7 +20,7 @@ const Skills = () => {
         <p className="text-2xl font-bold inline  text-center ">
           Web Development
         </p>
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 ">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4  text-center py-8 ">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
             <p className="my-4">HTML CSS</p>
           </div>
@@ -25,7 +28,14 @@ const Skills = () => {
             <p className="my-4">JavaScript</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <p className="my-4">React Js</p>
+            <p className="my-4">
+              React Js
+              <img
+                src={ReactLogo}
+                alt="React Logo"
+                className=" mx-auto h-6 w-6 float-right mr-[3rem] "
+              />
+            </p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <p className="my-4">Git Hub Pages</p>
@@ -34,7 +44,7 @@ const Skills = () => {
             <p className="my-4">NODE JS</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <p className="my-4">Weeb3.js</p>
+            <p className="my-4">Web3 js</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <p className="my-4">Yarn</p>
@@ -55,9 +65,7 @@ const Skills = () => {
             <p className="my-4">Flask</p>
           </div>
         </div>
-        <p className="text-2xl font-bold inline  text-center ">
-        Cybersecurity
-        </p>
+        <p className="text-2xl font-bold inline  text-center ">Cybersecurity</p>
         <div className="w-full grid grid-cols-2 sm:grid-cols-5 gap-4 text-center py-8 ">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <p className="my-4">Cisco Packet Tracer</p>
@@ -75,9 +83,7 @@ const Skills = () => {
             <p className="my-4">Open Sense</p>
           </div>
         </div>
-        <p className="text-2xl font-bold inline  text-center ">
-        Android
-        </p>
+        <p className="text-2xl font-bold inline  text-center ">Android</p>
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 ">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <p className="my-4">Java</p>
@@ -92,9 +98,7 @@ const Skills = () => {
             <p className="my-4">Android studio</p>
           </div>
         </div>
-        <p className="text-2xl font-bold inline  text-center ">
-        Data Science
-        </p>
+        <p className="text-2xl font-bold inline  text-center ">Data Science</p>
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 ">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <p className="my-4">NumPy</p>
@@ -118,9 +122,7 @@ const Skills = () => {
             <p className="my-4">IBM Quantum Learning</p>
           </div>
         </div>
-        <p className="text-2xl font-bold inline  text-center ">
-        Others
-        </p>
+        <p className="text-2xl font-bold inline  text-center ">Others</p>
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 ">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <p className="my-4">GIT</p>
